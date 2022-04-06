32d39c5f39dc3da984bd850a611c031e4941b6ba
Andrii Palatnyi <andriipal@KIE-X-L-0329.pt.playtech.corp>
2022-04-06 11:50:17 +0300

    paypal donate button

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function Requisite(props) {
           return (
             <div className="requisite-value">
               <div className="title">{item.label}</div>
-              <div className="value">{item.value}</div>
+              <div className="value">{item.customComponent ? <DonatePaypal/> : item.value}</div>
             </div>
           );
         })}
@@ -237,9 +237,7 @@ function HelpMatters() {
       <div className="requisities">
         {translate("requisities.ourRequisites")}
       </div>
-
       <DonateRequisities />
-
       <div className="donate-btn-holder">
         <div className="donate">
           {translate("requisities.donateDirectly")}
@@ -364,6 +362,18 @@ function Team(props) {
       <TeamMembers members={teamMembers} />
 
     </div>
+  );
+}
+
+function DonatePaypal() {
+  return (
+    <form action="https://www.paypal.com/donate" method="post" target="_top">
+      <input type="hidden" name="business" value="N8N4N934LR5WN" />
+      <input type="hidden" name="no_recurring" value="0" />
+      <input type="hidden" name="currency_code" value="USD" />
+      <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+      <img alt="" border="0" src="https://www.paypal.com/en_UA/i/scr/pixel.gif" width="1" height="1" />
+    </form>
   );
 }
 
--- src/Router.jsx
+++ src/Router.jsx
@@ -4,16 +4,13 @@ import { useBrowserLanguage } from './hooks';
 import Payment from './Payment.jsx';
 import App from './App.jsx';
 
-const supportedLang = { 'en': 'en', 'es': 'es', 'ua': 'ua' };
-
 function EdelweissRouter() {
     useBrowserLanguage();
 
     return (
         <Routes>
-            <Route path="/payment" element={<Payment />} />
-
             <Route path=":lang" element={<App />} />
+            {/* <Route path=":lang/paymesnt" element={<Payment />} /> */}
         </Routes>
     );
 }
